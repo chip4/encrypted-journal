e5f2a1dcb39a179d892e25a6e7c7b9e22c25190a
Chip <chip4@users.noreply.github.com>
2017-07-16 09:48:10 -0700

    use showdown markdown renderer

--- public/index.js
+++ public/index.js
@@ -1,12 +1,32 @@
 console.log("CodeMirror",CodeMirror);
+
+var converter = new showdown.Converter();
+
 CodeMirror.commands.save = function(instance) {
   console.log('SAVE', instance.doc.getValue());
-  document.getElementById("preview").innerHTML = markdown.toHTML(instance.doc.getValue());
+  document.getElementById("preview").innerHTML = converter.makeHtml(instance.doc.getValue());
 };
 
-console.log("markdown",markdown);
 
 var myCodeMirror = CodeMirror(document.getElementById("editor"), {
   mode: 'markdown',
   keyMap: 'vim',
+  value: `
+# h1
+## h2
+
+* bullet
+* bullet
+
+1. number
+2. number
+
+\`asdfa\`
+\`\`\`
+function(){
+  var a = 1;
+  return a++;
+}
+\`\`\`
+  `
 });
