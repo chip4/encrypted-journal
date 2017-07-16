8304762aaea2d1d1921ea7b82c4934b75120ba06
Chip <chip4@users.noreply.github.com>
2017-07-16 09:36:17 -0700

    preview working with markdown-js

--- public/index.js
+++ public/index.js
@@ -1,7 +1,10 @@
 console.log("CodeMirror",CodeMirror);
 CodeMirror.commands.save = function(instance) {
   console.log('SAVE', instance.doc.getValue());
+  document.getElementById("preview").innerHTML = markdown.toHTML(instance.doc.getValue());
 };
+
+console.log("markdown",markdown);
 
 var myCodeMirror = CodeMirror(document.getElementById("editor"), {
   mode: 'markdown',
