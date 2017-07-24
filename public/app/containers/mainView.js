8881c739a599aebbaf95ee44aff0934dd0829890
Chip <chip4@users.noreply.github.com>
2017-07-23 18:47:12 -0700

    Code Mirror disappears on re-render

--- public/app/containers/mainView.js
+++ public/app/containers/mainView.js
@@ -4,7 +4,26 @@ import showdown from '../externals/showdown.js';
 import codeMirrorElem from '../components/codeMirrorElem.js';
 var converter = new showdown.Converter();
 
+const prefix = 'editor';
+const changeEvent = `${prefix}:change`;
+const saveEvent = `${prefix}:save`;
+
+export function editorStore(state, emitter){
+  console.log("state",JSON.stringify(state.events.RENDER));
+  console.log("state.nav",state.events.NAVIGATE);
+  emitter.on(changeEvent, function(data){
+    console.log(changeEvent, data);
+  });
+  emitter.on(saveEvent, function(data){
+    console.log(saveEvent, data);
+    console.log("state.events",state.events);
+    console.log("state.events.RENDER",state.events.RENDER);
+    emitter.emit(state.events.RENDER);
+  });
+}
+
 export default function (state, emit){
+  console.log("state.events",state.events);
   return html`
     ${flexbox({flexDirection: 'column'},
       flexbox(
@@ -20,10 +39,12 @@ export default function (state, emit){
 
   function onChange(instance) {
     console.log('CHANGE');
+    emit(changeEvent, instance.doc.getValue());
     document.getElementById("preview").innerHTML = converter.makeHtml(instance.doc.getValue());
   };
 
   function onSave(instance){
     console.log('SAVE');
+    emit(saveEvent, instance.doc.getValue());
   }
 }
